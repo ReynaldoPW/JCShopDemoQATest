f36c6dacfbea5d9c77a67f044c48081c27fcfc0b
“reynaldo <reynaldopaulini@yahoo.com>
2023-02-26 10:15:18 +0700

    First Commit

--- src/main/java/com/juaracoding/selenium/Main.java
+++ src/main/java/com/juaracoding/selenium/Main.java
@@ -21,22 +21,29 @@ public class Main {
     }
 
     public static void testLogin(){
+        JavascriptExecutor js =(JavascriptExecutor) driver;
         driver.get("https://shop.demoqa.com");
 
         WebElement dismissButton = driver.findElement(By.xpath("/html/body/p/a"));
         dismissButton.click();
+        System.out.println("Dismiss button clicked");
 
         WebElement myAccountLink = driver.findElement(By.xpath("//*[@id=\"noo-site\"]/header/div[1]/div/ul[2]/li[2]/a"));
         myAccountLink.click();
+        System.out.println("MyAccount clicked");
 
+        js.executeScript("window.scrollBy(0,500)");
         WebElement usernameInput = driver.findElement(By.id("username"));
         usernameInput.sendKeys("reypau");
+        System.out.println("Username inputted");
 
         WebElement passwordInput = driver.findElement(By.id("password"));
         passwordInput.sendKeys("formeonly123");
+        System.out.println("Password Inputted");
 
         WebElement loginButton = driver.findElement(By.name("login"));
         loginButton.click();
+        System.out.println("Login Button clicked");
 
     }
 
@@ -46,17 +53,23 @@ public class Main {
 
         WebElement productLink = driver.findElement(By.cssSelector(".noo-product-inner"));
         productLink.click();
+        System.out.println("Product link clicked");
+
         js.executeScript("window.scrollBy(0,400)");
 
         WebElement selectColor = driver.findElement(By.id("pa_color"));
         Select color = new Select(selectColor);
         color.selectByValue("pink");
+        System.out.println("Color pink selected");
+
         WebElement selectSize = driver.findElement(By.id("pa_size"));
         Select size = new Select(selectSize);
         size.selectByValue("37");
+        System.out.println("Size 37 selected");
 
         WebElement addToCartButton = driver.findElement(By.cssSelector(".single_add_to_cart_button"));
         addToCartButton.click();
+        System.out.println("Add to cart button clicked");
 
     }
 
--- src/test/java/TestShopDemo.java
+++ src/test/java/TestShopDemo.java
@@ -11,10 +11,10 @@ import org.testng.annotations.Test;
 public class TestShopDemo {
     WebDriver driver;
     JavascriptExecutor js;
-
+    String pathChromeDriver ="C:\\selenium\\chromedriver_win32\\chromedriver.exe";
     @BeforeTest
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "C:\\selenium\\chromedriver_win32\\chromedriver.exe");
+        System.setProperty("webdriver.chrome.driver",pathChromeDriver);
         driver = new ChromeDriver();
         driver.manage().window().maximize();
         js = (JavascriptExecutor) driver;
@@ -26,18 +26,25 @@ public class TestShopDemo {
 
         WebElement dismissButton = driver.findElement(By.xpath("/html/body/p/a"));
         dismissButton.click();
+        System.out.println("dismiss button clicked");
 
         WebElement myAccountLink = driver.findElement(By.xpath("//*[@id=\"noo-site\"]/header/div[1]/div/ul[2]/li[2]/a"));
         myAccountLink.click();
+        System.out.println("myAccount clicked");
+
+        js.executeScript("window.scrollBy(0,500)");
 
         WebElement usernameInput = driver.findElement(By.id("username"));
         usernameInput.sendKeys("reypau");
+        System.out.println("Username inputted");
 
         WebElement passwordInput = driver.findElement(By.id("password"));
         passwordInput.sendKeys("formeonly123");
+        System.out.println("Password inputted");
 
         WebElement loginButton = driver.findElement(By.name("login"));
         loginButton.click();
+        System.out.println("Login button clicked");
     }
 
     @Test(priority = 2)
@@ -46,23 +53,27 @@ public class TestShopDemo {
 
         WebElement productLink = driver.findElement(By.cssSelector(".noo-product-inner"));
         productLink.click();
+        System.out.println("Product link clicked");
 
         js.executeScript("window.scrollBy(0,400)");
 
         WebElement selectColor = driver.findElement(By.id("pa_color"));
         Select color = new Select(selectColor);
         color.selectByValue("pink");
+        System.out.println("Color pink selected");
 
         WebElement selectSize = driver.findElement(By.id("pa_size"));
         Select size = new Select(selectSize);
         size.selectByValue("37");
+        System.out.println("Size 37 selected");
 
         WebElement addToCartButton = driver.findElement(By.cssSelector(".single_add_to_cart_button"));
         addToCartButton.click();
+        System.out.println("Add to cart button clicked");
     }
 
     @AfterTest
-    public void tearDown() {
+    public void quitBrowser() {
         driver.quit();
         System.out.println("Test finished");
     }
